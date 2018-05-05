da33da20f86c80b5c8e9780daa783770d7fcca17
Sathors <sathors@free.fr>
2018-05-05 13:01:20 -0500

    The library is working.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import 'babel-polyfill'
+import tmp from 'tmp'
+import scribble from 'scribbletune'
+import { exec } from 'child_process'
 
-console.log('coucou')
+const clip = scribble.clip({
+  notes: 'F#m C#m Dmaj Bm Emaj Amaj Dmaj C#m Amaj',
+  pattern: 'x_x_x_--'.repeat(8)
+})
+
+const playClip = (clip) => {
+  const temporaryFileName = tmp.fileSync().name
+  scribble.midi(clip, temporaryFileName)
+  exec(`timidity ${temporaryFileName}`)
+}
+
+playClip(clip)
